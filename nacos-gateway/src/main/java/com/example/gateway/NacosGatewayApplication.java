292252f89376e2d7a2cc45a8574603f110565a7f
zmc <zhangmc895@gmail.com>
2025-12-30 17:24:39 +0800

    fix:rename pacage name

--- nacos-gateway/src/main/java/com/example/gateway/NacosGatewayApplication.java
+++ nacos-gateway/src/main/java/com/example/gateway/NacosGatewayApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.example.gateway;
+package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
